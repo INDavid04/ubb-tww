5d9a89c3c82b323a53e64b2149d1f497752ea7e5
INDavid04 <davidirimia04@gmail.com>
2025-01-23 19:03:03 +0200

    show two random movies

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,12 +10,11 @@ function trimiteInformatiile(event) {
 
     // Adaugam filmul in local storage
     const films = JSON.parse(localStorage.getItem("films")) || [];
-    films.push({ title, description, link }); // desi films e constanta, putem adauga setul in lista pentru ca nu modificam adresa acesteia
+    // Desi films e constanta, putem adauga setul in lista pentru ca nu modificam adresa acesteia
+    films.push({ title, description, link });
     localStorage.setItem("films", JSON.stringify(films));
 
     afiseazaFilm(title, description, link);
-
-    // Resetam formularul
     event.target.reset();
 }
 
@@ -29,11 +28,9 @@ function afiseazaFilm(title, description, link) {
     const svgContainer = document.createElement("div");
     const div = document.createElement("div");
 
-    // Adaugam titlul și descrierea.
+    // Adaugam informatiile
     h3.textContent = title;
     p.textContent = description;
-
-    // Adaugam link-ul `Vezi filmul!`.
     a.setAttribute("href", link);
     a.setAttribute("target", "_blank");
     a.setAttribute("rel", "noopener noreferrer");
@@ -65,6 +62,91 @@ function afiseazaFilm(title, description, link) {
     li.style.padding =  "2rem 1rem";
 
     a.style.marginTop = "auto";
+}
+
+function afiseazaFilmeRandom() {
+    // Luam lista de filme din local storage
+    const films = JSON.parse(localStorage.getItem("films")) || [];
+
+    // Verificam sa avem cel putin doua filme
+    if (films.length < 2) {
+        alert("[RO] Trebuie să adaugi cel putin doua filme pentru a folosi aceasta functionalitate!\n[EN] You need to add at least two movies to use this feature!");
+        return;
+    }
+
+    // Randomizarea
+    const randomIndexes = [];
+    while (randomIndexes.length < 2) {
+        const randomIndex = Math.floor(Math.random() * films.length);
+        if (!randomIndexes.includes(randomIndex)) {
+            randomIndexes.push(randomIndex);
+        }
+    }
+
+    // Extragem filmele alese aleatoriu
+    const randomFilms = randomIndexes.map(index => films[index]);
+    const randomFilmsContainer = document.querySelector(".RandomFilms");
+
+    // Daca am mai generat filme atunci le vom scoate pentru a face loc celor noi
+    const existingList = randomFilmsContainer.querySelector("ol");
+    if (existingList) {
+        randomFilmsContainer.removeChild(existingList);
+    }
+
+    const ol = document.createElement("ol");
+    randomFilms.forEach(({ title, description, link }) => {
+        const li = document.createElement("li");
+        const h3 = document.createElement("h3");
+        const p = document.createElement("p");
+        const a = document.createElement("a");
+        const svgContainer = document.createElement("div");
+        const div = document.createElement("div");
+
+        h3.textContent = title;
+        p.textContent = description;
+
+        svgContainer.innerHTML = localStorage.getItem("svg");
+        a.appendChild(svgContainer);
+        a.appendChild(div);
+        a.setAttribute("href", link);
+        a.setAttribute("target", "_blank");
+        a.setAttribute("rel", "noopener noreferrer");
+        div.innerHTML = "Vezi filmul!";
+
+        li.appendChild(h3);
+        li.appendChild(p);
+        li.appendChild(a);
+        ol.appendChild(li);
+
+        // Styling
+        ol.style.padding = "0";
+        ol.style.display = "flex";
+        ol.style.flexWrap = "wrap";
+        ol.style.justifyContent = "center";
+        ol.style.alignItems = "flex-start";
+        ol.style.gap = "1rem";
+
+        li.style.display = "flex";
+        li.style.flexDirection = "column";
+        li.style.width = "18rem";
+        li.style.height = "24rem";
+        li.style.border = "1px solid #6A1E55";
+        li.style.listStyle = "none";
+        li.style.padding =  "2rem 1rem";
+
+        a.style.marginTop = "auto";
+        p.style.margin = "0";
+    });
+
+    randomFilmsContainer.appendChild(ol);
+}
+
+function keyDownListener(event) {
+    switch(event.key) {
+        case 'r':
+            afiseazaFilmeRandom();
+        break;
+    }
 }
 
 window.onload = function () {
@@ -106,5 +188,9 @@ window.onload = function () {
         afiseazaFilm(title, description, link);
     });
 
+    // Trimitem formularul prin apasarea butonului Trimite informatiile
     document.querySelector(".AddForm").addEventListener("submit", trimiteInformatiile);
+
+    // Daca apasam o tasta (r) afisam doua filme random
+    document.body.addEventListener('keydown', keyDownListener);
 };
